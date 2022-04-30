360bfe0481a649c97d0d2276aa559ec608a83f3b
Jacob Egestad <jacob.egestad@live.com>
2022-04-30 14:46:53 -0700

    fixed adding, did queries

--- backend/Backend.java
+++ backend/Backend.java
@@ -6,7 +6,7 @@
  * Description:
  * 
  * Known Bugs: N/A
- * Version: 17.0.1
+ * Java Version: 16.0.2
  */
 
 package backend;
@@ -50,7 +50,12 @@ public class Backend {
 	public static boolean addPatient(String fName, String lName, String bursarNum, String insurance, String birthday){
 		Statement stmt = dbConnect.createStatement();
 		ResultSet answer = stmt.executeQuery("SELECT MAX(PatientID) AS pid FROM Patient");
-		int newPatientId = answer.getInt("pid") + 1;
+		int newPatientId;
+		if(answer.next()){ // make sure this works
+			newPatientId = answer.getInt("pid") + 1;
+		} else{
+			newPatientId = 1;
+		}
 		int rowsAffected = stmt.executeUpdate(
 			String.format("INSERT INTO Patient VALUES (%d, '%s', '%s', %s, '%s', '%s')",
 				newPatientId, fName, lName, bursarNum, insurance, birthday));
@@ -61,7 +66,12 @@ public class Backend {
 	public static boolean addEmployee(int patientID, String acctNum, String routingNum){
 		Statement stmt = dbConnect.createStatement();
 		ResultSet answer = stmt.executeQuery("SELECT MAX(EmployeeID) AS eid FROM Employee");
-		int newEmpID = answer.getInt("eid") + 1;
+		int newEmpID;
+		if(answer.next()){ // make sure this works
+			newEmpID = answer.getInt("eid") + 1;
+		} else{
+			newEmpID = 1;
+		}
 		int rowsAffected = stmt.executeUpdate(
 			String.format("INSERT INTO Employee VALUES (%d, %d, %d, %d)",
 			newEmpID, patientID, acctNum, routingNum));
@@ -72,44 +82,131 @@ public class Backend {
 	public static boolean addAppointment(String checkinTime, String inPerson, String service, String empId, String patientID){
 		Statement stmt = dbConnect.createStatement();
 		ResultSet answer = stmt.executeQuery("SELECT MAX(ApptNo) AS ano FROM Appointment");
-		int newEmpID = answer.getInt("ano") + 1;
+		if(answer.next()){ // make sure this works
+			newApptNo = answer.getInt("ano") + 1;
+		} else{
+			newApptNo = 1;
+		}
 		int rowsAffected = stmt.executeUpdate(
 			String.format("INSERT INTO Appointment VALUES (%s, '%s', '%s', '%s', %s, %s)",
-			"" + newEmpID, checkinTime, inPerson, service, empId, patientID));
+			"" + newApptNo, checkinTime, inPerson, service, empId, patientID));
 		stmt.close();
 		return rowsAffected != 0;
 	}
 
 	public static boolean updatePatient(){
-
+		return false;
 	}
 
 	public static boolean updateEmployee(){
-
+		return false;
 	}
 	
 	public static boolean updateAppointment(){
-
+		return false;
 	}
 
 	public static boolean deletePatient(int patientId){
 		Statement stmt = dbConnect.createStatement();
-		int rowsAffected = stmt.executeUpdate(String.format("DELETE FROM Patient WHERE PatientId='%s'", patientId));
+		ResultSet answer = stmt.executeQuery(
+			String.format("SELECT COUNT(EmployeeID) FROM " + 
+			"Patient JOIN Employee USING (PatientID) WHERE PatientID=%d", patientId))
+		if(answer.next()){
+			System.err.println("Patient exists in Employeee table, remove employee first.");
+			return false;
+		}
+		int rowsAffected = stmt.executeUpdate(String.format("DELETE FROM Patient WHERE PatientId=%d", patientId));
 		stmt.close();
 		return rowsAffected != 0;
 	}
 
 	public static boolean deleteEmployee(int empId){
 		Statement stmt = dbConnect.createStatement();
-		int rowsAffected = stmt.executeUpdate(String.format("DELETE FROM Employee WHERE EmployeeId='%s'", empId));
+		int rowsAffected = stmt.executeUpdate(String.format("DELETE FROM Employee WHERE EmployeeId=%d", empId));
 		stmt.close();
 		return rowsAffected != 0;
 	}
 	
 	public static boolean deleteAppointment(int apptNo){
 		Statement stmt = dbConnect.createStatement();
-		int rowsAffected = stmt.executeUpdate(String.format("DELETE FROM Patient Appointment ApptNo='%s'", apptNo));
+		int rowsAffected = stmt.executeUpdate(String.format("DELETE FROM Patient Appointment ApptNo=%d", apptNo));
 		stmt.close();
 		return rowsAffected != 0;
+	}
+
+	public static boolean patientExists(int patientID){
+		Statement stmt = dbConnect.createStatement();
+		ResultSet answer = stmt.executeQuery(
+			String.format("SELECT COUNT(PatientID) AS numPatient FROM Patient WHERE PatientId=%d", patientID)
+		);
+		answer.next(); // make sure this works
+		int count = answer.getInt("numPatient");
+		stmt.close();
+		return count > 0;
+	}
+
+	public static ResultSet getPatientsScheduledForCOVIDImmunization(String date){ //q1
+		Statement stmt = dbConnect.createStatement();
+		ResultSet answer = stmt.executeQuery(
+			String.format("SELECT FName,LName FROM " +
+			"Patient JOIN Appointment USING (PatientID) JOIN Immunization USING (ApptNo) " + 
+			"JOIN Covid USING (INo) JOIN Scheduled USING (ApptNo)" +
+			"WHERE CONVERT(date, BookTime) <= '%s' AND DoseNo > 1 AND DoseNo < 5", date));
+		stmt.close();
+		return answer;
+	}
+
+	public static ResultSet getPatientsWithScheduledAppts(String date){ //q2
+		Statement stmt = dbConnect.createStatement();
+		ResultSet answer = stmt.executeQuery(
+			String.format("SELECT FName,LName FROM " +
+			"Patient JOIN Appointment USING (PatientID) JOIN Scheduled USING (ApptNo) " +
+			"WHERE CONVERT(date, BookTime) = '%s' " +
+			"GROUP BY Service ORDER BY BookTime", date)
+		);
+		stmt.close();
+		return answer;
+	}
+
+	public static ResultSet getStaffSchedule(){ //q3
+		Statement stmt = dbConnect.createStatement();
+		ResultSet answer = stmt.executeQuery(
+			"SELECT FName,LName,StartTime,EndTime FROM " +
+			"Patient JOIN Employee USING (PatientID) JOIN Shift USING (EmployeeID)"
+		);
+		stmt.close();
+		return answer;
+	}
+
+	public static ResultSet getCOVIDImmunizationStats(){ //q4
+		Statement stmt = dbConnect.createStatement();
+		ResultSet answer = stmt.executeQuery(
+			"SELECT COUNT(LName),MAX(DoseNo) AS DoseNo FROM " +
+			"Patient JOIN Appointment USING (PatientId) " +
+			"JOIN Immunization USING (ApptNo) JOIN Covid USING (INo) " +
+			"WHERE CheckinTime <= GETDATE()"
+		);
+		stmt.close();
+		return answer;
+	}
+
+	/**
+	 * getVaccinatedForIllness: returns the ResultSet of the names of people who have been vaccinated
+	 * for a given illness sorted by name
+	 * Pre-conditions: init has been called
+	 * Post-conditions: N/A
+	 * @param illness name of illness to check who has been vaccinated for it
+	 * @return ResultSet of list of people vaccinated for the illness
+	 */
+	public static ResultSet getVaccinatedForIllness(String illness){ //q5
+		Statement stmt = dbConnect.createStatement();
+		ResultSet answer = stmt.executeQuery(
+			String.format("SELECT FName,LName FROM " +
+			"Patient JOIN Appointment USING (PatientID) JOIN Immunization USING (ApptNo) " +
+			"WHERE IType='%s' AND CheckinTime <= GETDATE()" +
+			"ORDER BY Lname,FName", illness)
+		);
+		stmt.close();
+		return answer;
 	}
 }
